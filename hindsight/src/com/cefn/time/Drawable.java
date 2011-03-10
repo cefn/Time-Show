d32788a74efc6743461ef4c79f05bb03bc95f73f
Cefn Hoile <allyourx.github@cefn.com>
2011-03-10 02:40:50 +0000

    Removed spurious files

--- hindsight/src/com/cefn/time/Drawable.java
+++
@@ -1,7 +0,0 @@
-package com.cefn.time;
-
-import processing.core.PApplet;
-
-public interface Drawable {
-	void draw(PApplet applet);
-}
--- hindsight/src/com/cefn/time/Year.java
+++
@@ -1,9 +0,0 @@
-package com.cefn.time;
-
-import java.io.File;
-import java.util.List;
-
-public interface Year {
-	int getName();
-	List<File> getVideoFiles();
-}
