332ec8fb2f65212ce17ed5951ea386adb79df5c4
Cefn Hoile <cefn@cefn-ubuntu-samsung.(none)>
2011-05-13 18:02:13 +0100

    Changed location of fullscreen library

--- hourglass/launch.sh
+++ hourglass/launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Djava.library.path=libs/opengl/:libs/serial/ -classpath libs/serial/serial.jar:libs/serial/RXTXcomm.jar:libs/fullscreen/library/fullscreen.jar:libs/processing/core.jar:libs/opengl/opengl.jar:libs/opengl/jogl.jar:libs/opengl/gluegen-rt.jar:libs/jbox2d/jbox2d-2.0.1-library-only.jar:bin com.cefn.time.hourglass.Box2dApp
+java -Djava.library.path=libs/opengl/:libs/serial/ -classpath libs/serial/serial.jar:libs/serial/RXTXcomm.jar:libs/fullscreen/fullscreen.jar:libs/processing/core.jar:libs/opengl/opengl.jar:libs/opengl/jogl.jar:libs/opengl/gluegen-rt.jar:libs/jbox2d/jbox2d-2.0.1-library-only.jar:bin com.cefn.time.hourglass.Box2dApp
