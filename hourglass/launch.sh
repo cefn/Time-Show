a19d8ae5b1e50984c7e5f3c2c1c3db04076512a8
Cefn Hoile <cefn@cefn-ubuntu-samsung.(none)>
2011-07-10 13:12:03 +0100

    Added directory detection to ensure path can be introspected before launch.

--- hourglass/launch.sh
+++ hourglass/launch.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -Djava.library.path=libs/opengl/:libs/serial/ -classpath libs/serial/serial.jar:libs/serial/RXTXcomm.jar:libs/fullscreen/fullscreen.jar:libs/processing/core.jar:libs/opengl/opengl.jar:libs/opengl/jogl.jar:libs/opengl/gluegen-rt.jar:libs/jbox2d/jbox2d-2.0.1-library-only.jar:bin com.cefn.time.hourglass.Box2dApp
+cd `dirname $0`
+java -Djava.library.path=libs/opengl/:libs/serial/ -classpath "libs/minim/jsminim.jar:libs/minim/minim.jar:libs/minim/jl1.0.jar:libs/minim/tritonus_aos.jar:libs/minim/tritonus_share.jar:libs/minim/minim-spi.jar:libs/minim/mp3spi1.9.4.jar:libs/serial/serial.jar:libs/serial/RXTXcomm.jar:libs/fullscreen/fullscreen.jar:libs/processing/core.jar:libs/opengl/opengl.jar:libs/opengl/jogl.jar:libs/opengl/gluegen-rt.jar:libs/jbox2d/jbox2d-2.0.1-library-only.jar:bin" com.cefn.time.hourglass.Box2dApp
